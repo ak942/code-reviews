4645926f596b84bd6f5ac82123c9864f51f9cd63
maggie weir <mlweir98@gmail.com>
2023-04-28 13:48:40 -0400

    added two_sum function

--- functions/two_sum.py
+++ functions/two_sum.py
@@ -1,2 +1,6 @@
 def twoSum(n, t):
-    pass
+    def twoSum(n, t):
+    for i in range(len(n)):
+        for j in range(i + 1, len(n)):
+            if n[j] == t - n[i]:
+                return [i, j]
